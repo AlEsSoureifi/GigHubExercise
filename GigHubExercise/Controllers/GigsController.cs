7140742164feb2e4eaf7d9a74102e7f9884aebfb
Alexis Es Soureifi <alexisfmpro2007@yahoo.com>
2022-06-25 17:45:28 +0300

    Create Add a Gif Form

---
+++ GigHubExercise/Controllers/GigsController.cs
@@ -0,0 +1,31 @@
+﻿using GigHubExercise.Models;
+using GigHubExercise.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace GigHubExercise.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
